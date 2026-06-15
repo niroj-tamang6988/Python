9174a8cc9c97c6bcb60f4c949415a59a6d2fa166
Niroj Tamang <niroj6988@gmail.com>
2026-06-15 14:26:48 +0545

    Python dictionary method learned

--- dictonary_python.py
+++ dictonary_python.py
@@ -12,3 +12,9 @@ print(info["name"])
 
 #dictionary methods
 print(info.keys()) #print all keys in dictionary
+print(info.values()) #print all values in dictionary
+print(info.items()) #print all key-value pairs in dictionary
+print(info.get("age")) #no error if key is not existed return none
+
+info.update({"city": "kathmandu"}) #add a new key-value pair
+print(info)
